d719f0b449e897025a8a136bb1f900f23543a05e
Jason Leo Baguio <jasonleobaguio@gmail.com>
2013-10-23 16:09:13 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prowess::Application.config.secret_token = '8751570047aa7b53d54f6f893512bc9e71ce660600646d424e67141ed67efd7dafdaeca8e04fb1c64cd0ad37ef2051767f9e1162372756c7ff46d7597aa99062'
