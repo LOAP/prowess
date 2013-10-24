7e89942e869eb9deabc9c015a18fcaf899d71ee9
Jason Leo Baguio <jasonleobaguio@gmail.com>
2013-10-24 12:23:51 -0400

    changed background body

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Prowess::Application.routes.draw do
   resources :messages
 
 
+  get "welcome/index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -52,7 +53,7 @@ Prowess::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => redirect("/messages/new")
+   root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
